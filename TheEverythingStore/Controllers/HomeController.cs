1136cb9c63b1ec36b26cb6e2258bd07c9010deb4
CynthiaPublico <Cynthia.Dejesus-Publico@MyGeorgian.ca>
2019-02-18 22:33:49 -0500

    Created database Updated Web.config connection string

--- TheEverythingStore/Controllers/HomeController.cs
+++ TheEverythingStore/Controllers/HomeController.cs
@@ -7,8 +7,15 @@ using TheEverythingStore.Models;
 
 namespace TheEverythingStore.Controllers
 {
+
     public class HomeController : Controller
     {
+
+        // connect to db to make a reference to DB connection class
+        //instantiate and globally connect to the db
+        private DbModel db = new DbModel();
+
+
         public ActionResult Index()
         {
             return View();
@@ -42,30 +49,34 @@ namespace TheEverythingStore.Controllers
             ViewBag.Products = products; */
 
             //create instances of product class
-            var products = new List<Product>();
+            //var products = new List<Product>();
 
-            //create a loop 
-            for (int i = 1; i <= 10; i++)
-            {
-                //instantiate a new product object/ constructor
-                Product product = new Product();
+            ////create a loop 
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    //instantiate a new product object/ constructor
+            //    Product product = new Product();
 
-                //set name property
-                product.Name = "Product " + i.ToString();
+            //    //set name property
+            //    product.Name = "Product " + i.ToString();
 
-                //add the object to the list
-                products.Add(product);
-            }
+            //    //add the object to the list
+            //    products.Add(product);
+            //}
+
+            //use db object to create the database 
+            //use theproduct model to retrieve the entire product list from the sql server
+            var products = db.Products.ToList();
 
             //load the view and pass the product list to it
             return View(products);
         }
 
-        public ActionResult ViewProduct(string Productname)
+        public ActionResult ViewProduct(string ProductName)
         {
             //pass the string to the view using ViewBag since it is a single string value 
             //but can also use strongly type
-            ViewBag.ProductName = Productname;
+            ViewBag.ProductName = ProductName;
             return View();
         }
     }
