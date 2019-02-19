d90f00436aca288e1e87e7a8aadcaa687043c362
CynthiaPublico <Cynthia.Dejesus-Publico@MyGeorgian.ca>
2019-02-18 20:59:10 -0500

    Added Product  controller, model and view link

--- TheEverythingStore/Controllers/HomeController.cs
+++ TheEverythingStore/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TheEverythingStore.Models;
 
 namespace TheEverythingStore.Controllers
 {
@@ -25,6 +26,39 @@ namespace TheEverythingStore.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Products()
+        {
+            /* OLD UNTYPED VIEWBAG CODE
+            var products = new List<string>();
+            //create mock products
+            for (int i = 1; i <= 10; i++) {
+
+                products.Add("Product " + i.ToString());
+            }
+
+            //pass mock products to the view for display
+            ViewBag.Products = products; */
+
+            //create instances of product class
+            var products = new List<Product>();
+
+            //create a loop 
+            for (int i = 1; i <= 10; i++)
+            {
+                //instantiate a new product object/ constructor
+                Product product = new Product();
+
+                //set name property
+                product.Name = "Product " + i.ToString();
+
+                //add the object to the list
+                products.Add(product);
+            }
+
+            //load the view and pass the product list to it
+            return View(products);
         }
     }
 }
